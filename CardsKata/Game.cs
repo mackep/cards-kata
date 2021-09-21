558453b6ba09caa197b220a68455ceb0bc726074
Marcus Parkkinen <marcusparkkinen@live.com>
2021-09-21 14:33:51 +0200

    Fix error in description

--- CardsKata/Game.cs
+++ CardsKata/Game.cs
@@ -18,15 +18,15 @@ namespace CardsKata
              * For scoring purposes, the suits are unordered while the values are ordered as given above, with 2 being the lowest and ace the highest value.
              * A poker hand consists of 5 cards dealt from the deck. Poker hands are ranked by the following partial order from lowest to highest.
              *
-             * Flush
-             *   Hand contains 5 cards of the same suit. Hands which are both flushes are ranked using the rules for High Card.
+             * High card
+             *   Hands which do not fit any higher category are ranked by the value of their highest card. If the highest cards have the same value, the hands are
+             *   ranked by the next highest, and so on.
              *
              * Three of a kind
              *   Three of the cards in the hand have the same value. Hands which both contain three of a kind are ranked by the value of the 3 cards.
              *
-             * High card
-             *   Hands which do not fit any higher category are ranked by the value of their highest card. If the highest cards have the same value, the hands are
-             *   ranked by the next highest, and so on.
+             * Flush
+             *   Hand contains 5 cards of the same suit. Hands which are both flushes are ranked using the rules for High Card.
              *
              * Your job is to rank pairs of poker hands and to indicate which, if either, has a higher rank.
              * Note that it is possible to receive tie as result.
